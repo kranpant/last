a31e82553a975a0321e7c6796cfa2fb03f708feb
kranpant <110833432+kranpant@users.noreply.github.com>
2025-12-15 08:46:00 +0530

    Update TOKEN in config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 class Config:
     API_ID = os.environ.get("API_ID","20790743")
     API_HASH = os.environ.get("API_HASH","266b46661c0eb26ee0cb9ef7dfebfe39")
-    TOKEN = os.environ.get("TOKEN","7899518730:AAHnxSCu9MWam7DHOL8w-CnHnMeg_NEAjl4")
+    TOKEN = os.environ.get("TOKEN","7899518730:AAHS5rysk4RXgp82avZIuSSML33q9-wTWk0")
     START_PIC = os.environ.get("START_PIC")
     CHAT = os.environ.get("CHAT")
 
